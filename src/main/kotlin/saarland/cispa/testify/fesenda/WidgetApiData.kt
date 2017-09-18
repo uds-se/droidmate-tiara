7040eb44078a02a2b4787b1c4839e245f93dca5f
natanieljr <Nataniel.123>
2017-09-18 10:25:35 +0200

    Replaced equals function to consider only the API

--- src/main/kotlin/saarland/cispa/testify/fesenda/WidgetApiData.kt
+++ src/main/kotlin/saarland/cispa/testify/fesenda/WidgetApiData.kt
@@ -2,4 +2,17 @@ package saarland.cispa.testify.fesenda
 
 import org.droidmate.apis.IApi
 
-data class WidgetApiData(val api: IApi, val screenShot: String)
+data class WidgetApiData(val api: IApi, val screenShot: String){
+    override fun equals(other: Any?): Boolean {
+        if ((other == null) || (other !is WidgetApiData))
+            return false
+
+        return this.api.uniqueString == other.api.uniqueString
+    }
+
+    override fun hashCode(): Int {
+        var result = api.hashCode()
+        result = 31 * result + screenShot.hashCode()
+        return result
+    }
+}
