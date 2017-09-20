18e7f368ae004eefc878e25a553a4a21a52a80a2
natanieljr <Nataniel.123>
2017-09-20 09:45:03 +0200

    Make CandidateTrace serializable

--- src/main/kotlin/saarland/cispa/testify/fesenda/CandidateTrace.kt
+++ src/main/kotlin/saarland/cispa/testify/fesenda/CandidateTrace.kt
@@ -3,9 +3,14 @@ package saarland.cispa.testify.fesenda
 import org.droidmate.apis.IApi
 import org.droidmate.device.datatypes.Widget
 import saarland.cispa.testify.strategies.playback.PlaybackTrace
+import java.io.Serializable
 
-data class CandidateTrace(val widget: Widget, val trace: PlaybackTrace, val api: IApi){
+data class CandidateTrace(val widget: Widget, val trace: PlaybackTrace, val api: IApi): Serializable{
     var confirmed = false
     var blocked = false
     var similarityRatio = 0.0
+
+    companion object {
+        @JvmStatic private val serialVersionUID = 1
+    }
 }
